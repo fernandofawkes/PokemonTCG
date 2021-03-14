484ce27c4e8949a8534a3488314da21c2265e968
Fernando Malaman <fernandomalaman@brq.com>
2021-03-13 23:14:34 -0300

    card detail

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from 'src/app/models/Card';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  @Input() card: Card;
   constructor() { }
 
   ngOnInit(): void {
+
+    
   }
 
 }
